c769c70199ade9e0d64c200bd2be35105af3701d
francis-collab <f.mutabazi1@alustudent.com>
2025-06-22 17:55:56 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -21,16 +21,8 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+        """Return list of City instances related to current State."""
+        from models import storage
+        from models.city import City
+        return [city for city in storage.all(City).values() if city.state_id == self.id]
 
